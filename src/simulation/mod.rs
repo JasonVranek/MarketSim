e21d06da4f4dd617986a512b015a97ca08997462
Jason Vranek <jvranek@ucsc.edu>
2019-11-22 00:29:25 -0800

    basic history/logging functionality

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -3,3 +3,4 @@ pub mod random_behavior;
 pub mod simulation_config;
 pub mod simulation;
 pub mod config_parser;
+pub mod simulation_history;
--- src/simulation/simulation_history.rs
+++ src/simulation/simulation_history.rs
@@ -1,33 +1,53 @@
-use flowrs::order::Order;
-use flowrs::order::order_book::Book;
-use flowrs::utlity::get_time_now;
+use crate::order::order::{Order, TradeType};
+use crate::order::order_book::Book;
+use crate::utility::get_time;
+use std::collections::HashMap;
+use std::sync::Mutex;
+use std::time::Duration;
 
+
+// Tracks the essential information from an order in the order book
 pub struct Entry {
 	pub order_id: u64,
 	pub quantity: f64,	// Only thing that changes with order
 	pub timestamp: Duration,
 }
 
+impl Entry {
+	pub fn new(order_id: u64, quantity: f64) -> Self {
+		Entry {
+			order_id: order_id,
+			quantity: quantity,
+			timestamp: get_time(),
+		}
+	}
+}
+
 // Shallow copy of each order
-pub struct ShallowBook { // this should be a vec 
-	pub books: Mutex<HashMap<Vec<Entry>>>>,
-	pub block_num: u32,
+pub struct ShallowBook { 
+	pub orders: Vec<Entry>,
+	pub block_num: u64,
+	pub book_type: TradeType,
 }
 
 impl ShallowBook {
-	pub fn new(order_id, quantity) -> Self {
+	pub fn new(bid_or_ask: TradeType, num: u64) -> Self {
 		ShallowBook {
-			order_id: order_id,
-			quantity: quantity,
-			timestamp: get_time_now(),
+			orders: Vec::new(),
+			block_num: num,
+			book_type: bid_or_ask,
 		}
+	}
+
+	pub fn new_entry(&mut self, e: Entry) {
+		self.orders.push(e);
 	}
 }
 
 // A struct to track the state of the simulation for logging
 // and player strategies
 pub struct History {
-	pub mempool_data: Mutex<Hashmap<u64, Order>>,
+	pub mempool_data: Mutex<HashMap<u64, (Order, Duration)>>,
 	pub order_books: Mutex<Vec<ShallowBook>>,
 }
 
@@ -40,13 +60,44 @@ impl History {
 		}
 	}
 
+	// Adds an order indexed by its order id to a history of all orders to mempool 
 	pub fn mempool_order(&self, order: Order) {
 		let mut pool = self.mempool_data.lock().expect("History mempool lock");
-		pool.push(order);
+		pool.insert(order.order_id, (order, get_time()));
 	}
 
-	pub fn orderbook_change(&self, new_book: Book) {
-		let mut pool = self.order_books.lock().expect("History mempool lock");
-		pool.push(order);
+	// Parses through the orders and creates a shallow clone of the book
+	pub fn clone_book_state(&self, new_book: Vec<Order>, book_type: TradeType, block_num: u64) {
+		// Parse the orders into a ShallowBook 
+		let mut new_book_state = ShallowBook::new(book_type, block_num);
+		for order in new_book.iter() {
+			new_book_state.new_entry(Entry::new(order.order_id, order.quantity));
+		}
+
+		let mut prev_histories = self.order_books.lock().expect("History mempool lock");
+		prev_histories.push(new_book_state);
+	}
+
+	// Searches the hashmap of mempool orders
+	// Returns a copy of the order and the timestamp it was sent
+	pub fn find_orig_order(&self, order_id: u64) -> Option<(Order, Duration)> {
+		let mempool_data = self.mempool_data.lock().expect("find_orig_order");
+		match mempool_data.get(&order_id) {
+			Some((order, time)) => {
+				Some((order.clone(), time.clone()))
+			}
+			None => None,
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
